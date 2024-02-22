7358433ee7580aedfee99a2c8ee0f37f5a0708a2
urielsalis <uriel@urielsalis.me>
2024-02-22 19:59:34 +0100

    Stage #3: Respond to multiple PINGs

--- src/main/kotlin/com/urielsalis/codecrafters/redis/Client.kt
+++ src/main/kotlin/com/urielsalis/codecrafters/redis/Client.kt
@@ -3,17 +3,16 @@ package com.urielsalis.codecrafters.redis
 import java.net.Socket
 
 class Client(val clientSocket: Socket) {
+    private val connectionManager = ConnectionManager(clientSocket)
     fun handle() {
-        val input = clientSocket.getInputStream().bufferedReader()
-        val output = clientSocket.getOutputStream()
         while (true) {
-            val read = input.readLine()
-            if (read == null) {
-                clientSocket.close()
+            val message = connectionManager.readMessage()
+            if (message == null) {
+                connectionManager.close()
                 return
             }
-            if (read.lowercase().equals("ping")) {
-                output.write("+PONG\r\n".toByteArray())
+            if (message.lowercase().equals("ping")) {
+                connectionManager.sendMessage("+PONG\r\n".toByteArray())
             }
         }
     }
---
+++ src/main/kotlin/com/urielsalis/codecrafters/redis/ConnectionManager.kt
@@ -0,0 +1,26 @@
+package com.urielsalis.codecrafters.redis
+
+import java.io.BufferedReader
+import java.io.Closeable
+import java.io.OutputStream
+import java.net.Socket
+
+class ConnectionManager(val socket: Socket): Closeable {
+    private val input: BufferedReader = socket.getInputStream().bufferedReader()
+    private val output: OutputStream = socket.getOutputStream()
+
+    fun readMessage(): String? {
+        return input.readLine()
+    }
+
+    fun sendMessage(message: ByteArray) {
+        output.write(message)
+        output.flush()
+    }
+
+    override fun close() {
+        input.close()
+        output.close()
+        socket.close()
+    }
+}
