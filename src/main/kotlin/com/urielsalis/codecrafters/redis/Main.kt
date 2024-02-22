6fa0a6828be621df6bd34d79164da7e2350cf8ed
urielsalis <uriel@urielsalis.me>
2024-02-22 21:02:19 +0100

    Replication > Stage #1: Configure listening port

--- src/main/kotlin/com/urielsalis/codecrafters/redis/Main.kt
+++ src/main/kotlin/com/urielsalis/codecrafters/redis/Main.kt
@@ -1,16 +1,25 @@
 package com.urielsalis.codecrafters.redis
 
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.int
 import com.urielsalis.codecrafters.redis.storage.InMemoryStorage
 import java.net.ServerSocket
 import kotlin.concurrent.thread
 
-fun main(args: Array<String>) {
-    val port = 6379
-    val serverSocket = ServerSocket(port)
-    serverSocket.setReuseAddress(true)
 
-    val storage = InMemoryStorage()
+class RedisServer : CliktCommand() {
+    private val port by option("-p", "--port", help = "Port to listen to").int().default(6379)
+    override fun run() {
+        val serverSocket = ServerSocket(port)
+        serverSocket.setReuseAddress(true)
 
-    val serverManager = Server(serverSocket, storage)
-    thread { serverManager.acceptConnectionsLoop() }
+        val storage = InMemoryStorage()
+
+        val serverManager = Server(serverSocket, storage)
+        thread { serverManager.acceptConnectionsLoop() }
+    }
 }
+
+fun main(args: Array<String>) = RedisServer().main(args)
